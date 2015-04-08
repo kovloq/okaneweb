9bd0806ce10657c66e51da442a8c23209ab0074e
Dana <perdana_adhitama@yahoo.com>
2015-04-08 19:04:39 +0900

    Add new files

---
+++ app/controllers/transaction_controller.rb
@@ -0,0 +1,13 @@
+class TransactionController < ApplicationController
+
+	def index
+		render :layout => 'dashboard'
+	end
+
+	def trans_action
+	end
+
+	def add
+
+	end
+end
---
+++ db/migrate/20150320125910_create_transactions.rb
@@ -0,0 +1,17 @@
+class CreateTransactions < ActiveRecord::Migration
+  def change
+    create_table :transactions do |t|
+      t.integer :user_id
+      t.string :transaction_name
+      t.integer :category
+      t.integer :value
+      t.datetime :created_at
+      t.integer :created_by
+      t.datetime :updated_at
+      t.integer :updated_by
+      t.boolean :delete_flag
+
+      t.timestamps
+    end
+  end
+end
