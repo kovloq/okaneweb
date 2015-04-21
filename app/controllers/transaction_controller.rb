58d3ce0c87436edd24f716ff7ceed2df25a8d5c0
Dana <perdana_adhitama@yahoo.com>
2015-04-21 23:45:26 +0900

    error

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,13 +1,33 @@
 class TransactionController < ApplicationController
 
 	def index
+		@transaction = Transaction.order(:id => :desc).page(params[:page]).per(1) 
 		render :layout => 'dashboard'
 	end
 
-	def trans_action
+	def show
+		@transaction = Transaction.find params[:id]
+		render :layout => 'dashboard'
 	end
 
-	def add
-
+	def create
+		@transaction = Transaction.new(params[:transaction])
+	    if @transaction.save
+	        redirect_to @transaction, notice: "Successfully created."
+	    else
+	      render :action => 'add'
+	      ender :layout => 'dashboard'
+	    end
 	end
+
+	def new
+		@transaction = Transaction.new
+		render :layout => 'dashboard'
+	end
+
+	private
+
+  	def transaction_params
+    	params.require(:transaction).permit(:transaction_name, :category,:value, :created_by,:updated_by)
+  	end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,4 @@
 class Transaction < ActiveRecord::Base
+	# attr_accessible :transaction_name, :category,:value, :created_by,:updated_by,:deleted_flag 
 	belongs_to :user
 end
