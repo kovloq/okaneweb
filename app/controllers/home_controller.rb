308699e37c6c95ea6fb2768675aa1694d310b945
Dana <perdana_adhitama@yahoo.com>
2015-05-24 17:37:17 +0900

    Add contact Page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,24 @@
 class HomeController < ApplicationController
 
 	def index
+		# Email okanemanager@gmail.com
+		# pass !qazxsw@
+		# Twitter @okanemanager
+		# Pass !qazxsw@
+		# Google Plus https://plus.google.com/u/1/115652349374795853751/
+	end
+
+	def contact
+		@contact=Contact.new(contact_params)
+		if @contact.save
+		    redirect_to :controller => "home", :action => "index"
+		else
+		    render :action => 'index'
+		end
+	end
+
+	def transaction_params
+	    params.require(:contact).permit(:name, :email,:message, :phone)
 	end
 	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521091351) do
+ActiveRecord::Schema.define(version: 20150524082904) do
 
   create_table "admins", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.text     "message"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
