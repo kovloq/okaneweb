422befb1ea859ea2d75c3b16fe1b0199f62e2f44
Dana <perdana_adhitama@yahoo.com>
2015-08-19 22:17:56 +0900

    fix month

--- app/controllers/user/home_controller.rb
+++ app/controllers/user/home_controller.rb
@@ -6,8 +6,8 @@ class User::HomeController < UsersController
 	  	@income=Income.select("COUNT(*) as total,category_id").where("strftime('%m', date) = ? AND member_id = ? ",DateTime.now.strftime("%m"),current_member["id"]).group("category_id")
 	  	@expense=Expense.select("COUNT(*) as total,category_id").where("strftime('%m', date) = ? AND member_id = ? ",DateTime.now.strftime("%m"),current_member["id"]).group("category_id")
   	else
-  		@income=Income.select("COUNT(*) as total,category_id").where("MONTH(date) = ? AND member_id = ? ",DateTime.now.strftime("%m"),current_member["id"]).group("category_id")
-	  	@expense=Expense.select("COUNT(*) as total,category_id").where("MONTH(date) = ? AND member_id = ? ",DateTime.now.strftime("%m"),current_member["id"]).group("category_id")
+  		@income=Income.select("COUNT(*) as total,category_id").where("EXTRACT(MONTH FROM date) = ? AND member_id = ? ",DateTime.now.strftime("%m"),current_member["id"]).group("category_id")
+	  	@expense=Expense.select("COUNT(*) as total,category_id").where("EXTRACT(MONTH FROM date) = ? AND member_id = ? ",DateTime.now.strftime("%m"),current_member["id"]).group("category_id")
   	end
 
   end
