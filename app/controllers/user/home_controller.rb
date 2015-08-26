1b96d5c98951ae22c3f6f0bff4bfe39af53a12a1
Dana <perdana_adhitama@yahoo.com>
2015-08-26 17:37:21 +0900

    display error

--- app/controllers/user/home_controller.rb
+++ app/controllers/user/home_controller.rb
@@ -10,7 +10,7 @@ class User::HomeController < UsersController
   	else
   		@income=Transaction.select("COUNT(*) as total,category_id").where("EXTRACT(MONTH FROM date) = ? AND member_id = ? AND t_category = ? ",DateTime.now.strftime("%m"),current_member["id"],1).group("category_id")
 	  	@expense=Transaction.select("COUNT(*) as total,category_id").where("EXTRACT(MONTH FROM date) = ? AND member_id = ? AND t_category = ? ",DateTime.now.strftime("%m"),current_member["id"],2).group("category_id")
-      @tot_income=Transaction.select("SUM(amount) as tot").where("EXTRACT(MONTH FROM date) = ? AND member_id = ? AND t_category = ? ",DateTime.now.strftime("%m"),current_member["id"],1).first
+      @tot_income=Transaction.select("SUM(amount) as tot,id").where("EXTRACT(MONTH FROM date) = ? AND member_id = ? AND t_category = ? ",DateTime.now.strftime("%m"),current_member["id"],1).first
       @tot_expense=Transaction.select("SUM(amount) as tot,id").where("EXTRACT(MONTH FROM date) = ? AND member_id = ? AND t_category = ? ",DateTime.now.strftime("%m"),current_member["id"],2).first
 
       
