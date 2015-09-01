6c8925f19ef4d9c1fb6f7ba4e78b08527c0e5f90
Dana <perdana_adhitama@yahoo.com>
2015-09-01 22:42:27 +0900

    add oath

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,6 +4,23 @@ class Member < ActiveRecord::Base
   devise :database_authenticatable, :registerable,:confirmable,
          :recoverable, :rememberable, :trackable, :validatable,:omniauthable, :omniauth_providers => [:facebook,:twitter,:google_oauth2]
 
+	def self.from_omniauth(auth)
+	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+	    member.email = auth.info.email
+	    member.password = Devise.friendly_token[0,20]
+	    member.name = auth.info.name   # assuming the user model has a name
+	    member.image = auth.info.image # assuming the user model has an image
+	  end
+	end
+
+	def self.new_with_session(params, session)
+	   super.tap do |user|
+	      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+	        user.email = data["email"] if user.email.blank?
+	      end
+	    end
+	end
+
    	protected
 	def confirmation_required?
   		false
