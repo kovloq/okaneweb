a9dc7db88e59c2c105f33394e295abe7556fbb92
Dana <perdana_adhitama@yahoo.com>
2015-09-02 22:04:09 +0900

    add Twitter

--- app/controllers/members/omniauth_callbacks_controller.rb
+++ app/controllers/members/omniauth_callbacks_controller.rb
@@ -47,7 +47,7 @@ class Members::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @member, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
     else
-      session["devise.twitter_data"] = request.env["omniauth.auth"]
+      session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
       redirect_to new_member_registration_url
     end
   end
